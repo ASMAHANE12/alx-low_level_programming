b671771b86efb7be16df214cd203aa1130b5750f
ASMAHANE12 <harimechasmahane@gmail.com>
2023-07-18 18:48:59 -0700

    you know there are 11 huh

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * add - a function that adds two integers and return the result.
+ *
+ * @i: integer number as input.
+ * @c: integer number as input.
+ *
+ * Return: return the addition of the numbers.
+ */
+int add(int i, int c)
+{
+	return (i + c);
+	_putchar('\n');
+}
